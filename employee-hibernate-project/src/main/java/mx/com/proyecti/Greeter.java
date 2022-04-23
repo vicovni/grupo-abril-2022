c6f00eecd2d37b4ce857b0d9888e0cdc9d3e7776
Victor Calderon <vicovni@gmail.com>
2022-04-23 12:47:48 -0500

    Actualización parcial

--- employee-hibernate-project/src/main/java/mx/com/proyecti/Greeter.java
+++ employee-hibernate-project/src/main/java/mx/com/proyecti/Greeter.java
@@ -1,8 +1,10 @@
 package mx.com.proyecti;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
+//JAX-WS
 @WebService
 public class Greeter {
 	
@@ -12,9 +14,9 @@ public class Greeter {
 		
 	}
 	
-	@WebMethod
-	public String greetWorld() {
-		return message;
+	@WebMethod(operationName="greetHi")
+	public String greetWorld(@WebParam(name="FirstName") String name) {
+		return message + name;
 	}
 
 }
